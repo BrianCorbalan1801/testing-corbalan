a64f5a92551a4f1884b63003fc9600f00acaf9dc
Briancorbalan1801 <bcorbalanbenitez@gmail.com>
2024-08-14 09:01:28 -0300

    Completado el test de actualizar email

---
+++ examples/tutorial/tests/test_mod_user.py
@@ -0,0 +1,16 @@
+import pytest
+
+from flaskr.db import get_db
+
+def test_update_user_email(client, auth, app):
+    auth.login()
+
+    assert client.get("/usermod/update-mail").status_code == 200
+    client.post("/usermod/update-mail", data={"user_email": "x@x"})
+
+    with app.app_context():
+        db = get_db()
+        user = db.execute("SELECT * FROM user WHERE id = 1").fetchone()
+        assert user["user_email"] == "x@x"
+
+
